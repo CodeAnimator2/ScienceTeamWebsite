c181837f2ecb65e7b99cf7a31b77d773378fba81
Kevin Chen <kevinchen2003@gmail.com>
2011-10-21 20:36:17 -0700

    Initial commit.
    Beginnings of a Science Team website. Using PHP include for headers, @font-face for custom web fonts, and 960 Grid System for layout. And other cool things.

---
+++ g_header.php
@@ -0,0 +1,38 @@
+Amador Valley Science Team</title>
+	<!-- G_HEADER.PHP CUT HERE -->
+	<link rel="stylesheet" href="/css/960.css" />
+	<link rel="stylesheet" href="/css/master.css" />
+	
+	<!--[if lte IE 8]>
+	<meta http-equiv="REFRESH" content="0;url=/ie/" />
+	<![endif]-->
+	
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	
+	<meta name="viewport" content="width=1024,user-scalable=yes" />
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<link rel="apple-touch-icon-precomposed" href="/apple-touch-icon-precomposed.png" />
+	
+	<meta http-equiv="X-UA-Compatible" content="chrome=1" />
+</head>
+
+<body>
+<div class="container_16">
+	<header class="grid_5">
+		<a href="#" id="logo"></a>
+		<h1>Amador Valley Science Team</h1>
+		<ul id="nav">
+			<a href="#"><li>About the club</li></a>
+			<a href="#"><li>Events</li></a>
+			<ul>
+				<a href="#"><li>Science Olympiad</li></a>
+				<a href="#"><li>Academic Olympiad</li></a>
+				<a href="#"><li>Bay Area Puzzle Hunt</li></a>
+				<a href="#"><li>Science Bowl</li></a>
+			</ul>
+			<a href="#"><li>Gallery</li></a>
+			<a href="#"><li>Contact</li></a>
+		</ul>
+	</header>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>
+	<?php include 'http://www.avscienceteam.com/g_header.php'; ?>
+	<div class="grid_11">
+		<h2>The <em>Heading</em> for This Page Is A Heading 2</h2>
+		<h3>Hello, and again, <a href="#">welcome</a> to the</h3>
+		<p>You will be testing with a partner today.</p>
+		<p>Caroline, what do <a href="#">these people</a> buy? Why, kittens, <em>of course!</em> Helvetica american apparel blog, <strong>hoodie leggings portland</strong> etsy PBR pitchfork four loko next level photo booth. Bicycle rights williamsburg mustache locavore portland, banksy stumptown 8-bit photo booth 3 wolf moon.</p>
+		<p><em><strong>CATS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</strong></em></p>
+		<img src="http://placekitten.com/640/480" alt="CATZ" />
+		<img src="http://placekitten.com/640/300" alt="MOAR CATZ" />
+	</div>
+</div>
+<?php include 'http://www.avscienceteam.com/g_footer.php'; ?>
