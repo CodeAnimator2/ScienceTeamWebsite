17e98895c89d2496346c1abde8e4b8cff4bbea88
LeafShadow5 <stevenc2009@gmail.com>
2014-08-06 13:11:34 -0700

    Cynthia C., Thi, and Steven's Biographies updated

--- about/index.php
+++ about/index.php
@@ -19,16 +19,16 @@
 		<p>Biography coming soon&hellip;</p>
 		
 		<h4>Cynthia Chen, Vice President of Communications</h4>
-		<p>Biography coming soon&hellip;</p>
+		<p>Cynthia Chen is a senior who has been a part of Amador Valley Science Team since freshman year. Last year, she served as Treasurer, and she is excited to return as Vice President of Communications this year. She has participated in Science Olympiad since 8th grade and her favorite events are Forensics and Sounds of Music. The previous summer, she spent her time at the Women’s Technology Program in MIT. Apart from the STEM realm, she is also involved in band, DECA, and ballet.</p>
 		
 		<h4>Steven Chang, Vice President of Resources</h4>
-		<p>Biography coming soon&hellip;</p>
+		<p>Since his freshman year, Steven has competed in most of the academic pentathlon and science bowl. Now as a senior and VP of Resources, he is eager to help take charge of Amador Valley Science Team and lead it to a new direction. He welcomes all who share his enthusiasm for science and the challenges STEM brings. In addition to Science Team, he also enjoys being a part of SciFY, DECA, and badminton club.</p>
 		
 		<h4>Cynthia Tien, Treasurer</h4>
 		<p>Biography coming soon&hellip;</p>
 		
 		<h4>Thi Nguyen, Secretary</h4>
-		<p>Biography coming soon&hellip;</p>
+		<p>Thi is currently a junior who has been part of SciOly since 8th grade and has been on Team A since freshman year. Last year, she filled her time with marching band, Wind Ensemble I Concert band, CBDA, and DECA. She is excited to see Science Team evolve over time and is looking forward to helping elevate Science Team to new heights this upcoming year. Her favorite food is jasmine green tea with boba from Quickly's (and considers it a food because it involves chewing).</p>
 		
 		<h3>Meet the advisers.</h3>
 		<h4>Tom Hall, Physics teacher</h4>
