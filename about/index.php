a1617fb2864a96cbd28b35d66c56b05a6c43f8b6
LeafShadow5 <stevenc2009@gmail.com>
2014-08-06 13:35:14 -0700

    Made biography text smaller

--- about/index.php
+++ about/index.php
@@ -16,22 +16,22 @@
 		
 		<h3>Meet the officers of AVaST 2014-2015</h3>
 		<h4>Janel Lee, President</h4>
-		<p>Biography coming soon&hellip;</p>
+		<p><i>Biography coming soon&hellip;</i></p>
 		
 		<h4>Cynthia Chen, Vice President of Communications</h4>
-		<p>Cynthia Chen is a senior who has been a part of Amador Valley Science Team since freshman year. Last year, she served as Treasurer, and she is excited to return as Vice President of Communications this year. She has participated in Science Olympiad since 8th grade and her favorite events are Forensics and Sounds of Music. The previous summer, she spent her time at the Women’s Technology Program in MIT. Apart from the STEM realm, she is also involved in band, DECA, and ballet.</p>
+		<p><small>Cynthia Chen is a senior who has been a part of Amador Valley Science Team since freshman year. Last year, she served as Treasurer, and she is excited to return as Vice President of Communications this year. She has participated in Science Olympiad since 8th grade and her favorite events are Forensics and Sounds of Music. The previous summer, she spent her time at the Women’s Technology Program in MIT. Apart from the STEM realm, she is also involved in band, DECA, and ballet.</small></p>
 		
 		<h4>Steven Chang, Vice President of Resources</h4>
-		<p>Since his freshman year, Steven has competed in most of the academic pentathlon and science bowl. Now as a senior and VP of Resources, he is eager to help take charge of Amador Valley Science Team and lead it to a new direction. He welcomes all who share his enthusiasm for science and the challenges STEM brings. In addition to Science Team, he also enjoys being a part of SciFY, DECA, and badminton club.</p>
+		<p><small>Since his freshman year, Steven has competed in most of the academic pentathlon and science bowl. Now as a senior and VP of Resources, he is eager to help take charge of Amador Valley Science Team and lead it to a new direction. He welcomes all who share his enthusiasm for science and the challenges STEM brings. In addition to Science Team, he also enjoys being a part of SciFY, DECA, and badminton club.</small></p>
 		
 		<h4>Cynthia Tien, Treasurer</h4>
-		<p>Biography coming soon&hellip;</p>
+		<p><i>Biography coming soon&hellip;</i></p>
 		
 		<h4>Thi Nguyen, Secretary</h4>
-		<p>Thi is currently a junior who has been part of SciOly since 8th grade and has been on Team A since freshman year. Last year, she filled her time with marching band, Wind Ensemble I Concert band, CBDA, and DECA. She is excited to see Science Team evolve over time and is looking forward to helping elevate Science Team to new heights this upcoming year. Her favorite food is jasmine green tea with boba from Quickly's (and considers it a food because it involves chewing).</p>
+		<p><small>Thi is currently a junior who has been part of SciOly since 8th grade and has been on Team A since freshman year. Last year, she filled her time with marching band, Wind Ensemble I Concert band, CBDA, and DECA. She is excited to see Science Team evolve over time and is looking forward to helping elevate Science Team to new heights this upcoming year. Her favorite food is jasmine green tea with boba from Quickly's (and considers it a food because it involves chewing).</small></p>
 		
 		<h3>Meet the advisers.</h3>
 		<h4>Tom Hall, Physics teacher</h4>
-		<p>Biography coming soon&hellip;</p>
+		<p><i>Biography coming soon&hellip;</i></p>
 		
 <?php include $domain . 'g_footer.php'; ?>
