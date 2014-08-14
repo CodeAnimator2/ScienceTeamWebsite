112e2662af33e7069f3bf2e976af5fd463418691
LeafShadow5 <stevenc2009@gmail.com>
2014-08-14 16:20:13 -0700

    Fixed Gallery cellspacing and reordered pictures

--- gallery/index.php
+++ gallery/index.php
@@ -10,24 +10,25 @@
 	</style>
 	<title>Gallery<?php include $domain . 'g_header.php'; ?>
 		<h2>Gallery</h2>
-		<p>Content coming soon.</p>
-		<table cellspacing = "10">
+		<table cellspacing = "30">
+			<tr>
+				<td> <img src = "/img/gallery/Fresno2012.png" width = "660"> </td>
+			</tr>
+			<tr>
+				<td> <img src = "/img/gallery/ScienceTeam.png" width = "660"> </td>
+			</tr>
 			<tr>
 				<td> <img src = "/img/Gallery/2013_BuildEvent.png" width = "200"> </td>
-				<td> <img src = "/img/Gallery/Fresno2012.png" width = "200"> </td>
+				<td> <img src = "/img/Gallery/YashMaglev.png" width = "200"> </td>
 				<td> <img src = "/img/Gallery/Janel_CynthiaC.png" width = "200"> </td>
 			</tr>
 			<tr>
-				<td> <img src = "/img/Gallery/YashMaglev.png" width = "200"> </td>
+				<td> <img src = "/img/Gallery/JanelProject.png" width = "200"> </td>
 				<td> <img src = "/img/Gallery/JenniferBoomilever.png" width = "200"> </td>
 				<td> <img src = "/img/Gallery/PerryScrambler.png" width = "200"> </td>
 			</tr>
 			<tr>
 				<td> <img src = "/img/Gallery/PerryScrambler2.png" width = "200"> </td>
-				<td> <img src = "/img/Gallery/JanelProject.png" width = "200"> </td>
-				<td> <img src = "/img/Gallery/SparklyRose.png" width = "200"> </td>
-			</tr>
-			<tr>
-				<td> <img src = "/img/Gallery/ScienceTeam.png" width = "200"> </td>
+				<td> <img src = "/img/Gallery/SparklyRose.png" width = "400"> </td>
 			</tr>
 <?php include $domain . 'g_footer.php'; ?>
