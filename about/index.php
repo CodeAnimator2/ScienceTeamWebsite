09a7a191752f0694e03fb20c3bb795af82454da6
LeafShadow5 <stevenc2009@gmail.com>
2014-08-17 21:35:15 -0700

    Added sign-up link and rest of emails

--- about/index.php
+++ about/index.php
@@ -18,7 +18,7 @@
 		<ul class = "bio">
 			<li><h4>Janel Lee, President</h4>
 			    <p><small>Janel is a senior this year who has been with Science Team since she was a freshman. Last year, she served as Secretary and Captain of Science Olympiad Team Purple. She has participated in other competitions such as the USA Computing Olympiad and the Intel International Science & Engineering Fair. Her leadership experience stems from serving as an officer for other clubs and organizing events in the Academic Pentathlon for Science Team members.</p>
-		    	<p><small><i>"I greatly look forward to work with AVaST members to help them succeed in the various events our club has to offer. We're all part of one team, so whenever you have a question or need some help, don't be afraid to reach out to me at <a href = "mailto:janellee@avscienceteam.com">janellee@avscienceteam.com</a></small>!"</i></p>
+		    	<p><small><i>"I greatly look forward to work with AVaST members to help them succeed in the various events our club has to offer. We're all part of one team, so whenever you have a question or need some help, don't be afraid to reach out to me at </i><a href = "mailto:janellee@avscienceteam.com">janellee@avscienceteam.com</a></small>!"</p>
                         
 		    </li>
 		</ul>
@@ -26,26 +26,31 @@
 			<li><h4>Cynthia Chen, Vice President of Communications</h4>
 				<table cellspacing = "10">
 					<td><img src = "/img/CynthiaC.png" width = "175"></td>
-			    	<td><p><small>Cynthia Chen is a senior who has been a part of Amador Valley Science Team since freshman year. Last year, she served as Treasurer, and she is excited to return as Vice President of Communications this year. She has participated in Science Olympiad since 8th grade and her favorite events are Forensics and Sounds of Music. The previous summer, she spent her time at the Women’s Technology Program in MIT. Apart from the STEM realm, she is also involved in band, DECA, and ballet.</small></p></td>
+			    	<td><p><small>Cynthia Chen is a senior who has been a part of Amador Valley Science Team since freshman year. Last year, she served as Treasurer, and she is excited to return as Vice President of Communications this year. She has participated in Science Olympiad since 8th grade and her favorite events are Forensics and Sounds of Music. The previous summer, she spent her time at the Women’s Technology Program in MIT. Apart from the STEM realm, she is also involved in band, DECA, and ballet.</p>
+			    		<p>Contact: <a href = "mailto:cynthiachen@avscienceteam.com">cynthiachen@avscienceteam.com</a></small></p>
+			    	</td>
 				</table>
 			</li>
 		</ul>
 		<ul class = "bio">
 			<li><h4>Steven Chang, Vice President of Resources</h4>
 			    <p><small>Since his freshman year, Steven has competed in most of the academic pentathlon and science bowl. Now as a senior and VP of Resources, he is eager to help take charge of Amador Valley Science Team and lead it to a new direction. He welcomes all who share his enthusiasm for science and the challenges STEM brings. In addition to Science Team, he also enjoys being a part of SciFY, DECA, and badminton club.</p>
-                            <p>Contact: <a href = "mailto:stevenchang@avscienceteam.com">stevenchang@avscienceteam.com</a></small></p>
+                <p>Contact: <a href = "mailto:stevenchang@avscienceteam.com">stevenchang@avscienceteam.com</a></small></p>
 			</li>
 		</ul>
 		<ul class = "bio">
 			<li><h4>Cynthia Tien, Treasurer</h4>
 			    <p><small>Cynthia is a senior this year who has been a member of Science Team since sophomore year. She has participated in the Science Olympiad competitions for the past two years, last year serving as captain of Gold Team. Her leadership experience includes working closely with different elementary science teams in the area and organizing the first elementary science olympiad in Pleasanton. In her free time she enjoys playing water polo and swimming.</small></p>
+			    <p>Contact: <a href = "mailto:cynthiatien@avscienceteam.com">cynthiatien@avscienceteam.com</a></small></p>
 			</li>
 		</ul>
 		<ul class = "bio">
 			<li><h4>Thi Nguyen, Secretary</h4>
 				<table cellspacing = "10">
 					<td><img src = "/img/Thi.png" width = "175"></td>
-				    <td><p><small>Thi is currently a junior who has been part of SciOly since 8th grade and has been on Team A since freshman year. Last year, she filled her time with marching band, Wind Ensemble I Concert band, CBDA, and DECA. She is excited to see Science Team evolve over time and is looking forward to helping elevate Science Team to new heights this upcoming year. Her favorite food is jasmine green tea with boba from Quickly's (and considers it a food because it involves chewing).</small></p></td>
+				    <td><p><small>Thi is currently a junior who has been part of SciOly since 8th grade and has been on Team A since freshman year. Last year, she filled her time with marching band, Wind Ensemble I Concert band, CBDA, and DECA. She is excited to see Science Team evolve over time and is looking forward to helping elevate Science Team to new heights this upcoming year. Her favorite food is jasmine green tea with boba from Quickly's (and considers it a food because it involves chewing).</p>
+				    	<p>Contact: <a href = "mailto:thinguyen@avscienceteam.com">thinguyen@avscienceteam.com</a></small></p>
+				    </td>
 				</table>
 			</li>
 		</ul>
